ceff53683235993c6c6ef6acf7d696975a92c711
urli-by <urli.by@gmail.com>
2022-11-18 10:57:27 +0400

    lab-1

---
+++ lab-1/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "My_first_inst" {
+    ami = "ami-096800910c1b781ba"
+    instance_type = "t2.micro"
+
+    tags = {
+        learn = "terraform"
+        lab = "1"
+    } 
+}
+
+resource "aws_instance" "My_second_inst" {
+    ami = "ami-01cae1550c0adea9c"
+    instance_type = "t2.micro"
+
+    tags = {
+        learn = "terraform"
+        lab = "1"
+    }
+  
+}
