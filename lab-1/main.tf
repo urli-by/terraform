ec00704372e1a48f525b8a6b1657cdf19777b534
urli-by <urli.by@gmail.com>
2022-11-18 11:24:28 +0400

    edited gitignore

--- lab-1/main.tf
+++ lab-1/main.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "My_first_inst" {
-    ami = "ami-096800910c1b781ba"
-    instance_type = "t2.micro"
+    ami             = "ami-096800910c1b781ba"
+    instance_type   = "t2.micro"
 
     tags = {
-        learn = "terraform"
-        lab = "1"
+        learn       = "terraform"
+        lab         = "1"
     } 
 }
 
 resource "aws_instance" "My_second_inst" {
-    ami = "ami-01cae1550c0adea9c"
-    instance_type = "t2.micro"
-    key_name = "urli_terraform_key_18_11_2022"
+    ami             = "ami-01cae1550c0adea9c"
+    instance_type   = "t2.micro"
+    key_name        = "urli_terraform_key_18_11_2022"
     tags = {
-        learn = "terraform"
-        lab = "1"
+        learn       = "terraform"
+        lab         = "1"
     }
   
 }
