4f53471f8d818617ed66bd7bd96a5b566cf059fc
urli-by <urli.by@gmail.com>
2022-12-08 17:31:02 +0400

    lab-19

---
+++ lab-19/variables.tf
@@ -0,0 +1,29 @@
+variable "env" {
+  default = "prod"
+}
+
+variable "server_size" {
+  default = {
+    prod       = "t2.large"
+    staging    = "t2.medium"
+    dev        = "t2.small"
+    my_default = "t2.nano"
+  }
+}
+
+variable "ami_id_per_region" {
+  description = "My Custom AMI id per Region"
+  default = {
+    "us-west-2"  = "ami-0e472933a1395e172"
+    "us-west-1"  = "ami-08d9a394ac1c2994c"
+    "eu-west-1"  = "ami-0ce1e3f77cd41957e"
+    "ap-south-1" = "ami-08f63db601b82ff5f"
+  }
+}
+
+variable "allow_port" {
+  default = {
+    prod = ["80", "443"]
+    rest = ["80", "443", "8080", "22"]
+  }
+}
